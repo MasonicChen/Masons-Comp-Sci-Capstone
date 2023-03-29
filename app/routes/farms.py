71226e282ea6add30d71aba60b872d2a619f8571
MasonicChen <masonchen2004@gmail.com>
2023-03-29 12:44:12 -0700

    asddfjiopo;

--- app/routes/farms.py
+++ app/routes/farms.py
@@ -1,11 +1,40 @@
 from app import app
+from app.utils.secrets import getSecrets
 import mongoengine.errors
+import requests
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
 from app.classes.data import Farm 
 from app.classes.forms import FarmForm
 from flask_login import login_required
 import datetime as dt
+
+def updateLatLon(farm):
+    # get your email address for the secrets file
+    secrets=getSecrets()
+    # call the maps API with the address
+    url = f"https://nominatim.openstreetmap.org/search?street={farm.streetAddress}&city={farm.city}&state={farm.state}&postalcode={farm.zip}&format=json&addressdetails=1&email={secrets['MY_EMAIL_ADDRESS']}"
+    # get the response from the API
+    r = requests.get(url)
+    # Find the lat/lon in the response
+    try:
+        r = r.json()
+    except Exception as error:
+        flash(f"unable to retrieve lat/lon: {error}")
+        return(farm)
+    else:
+        if len(r) != 0:
+            # update the database
+            farm.update(
+                lat = float(r[0]['lat']),
+                lon = float(r[0]['lon'])
+            )
+            flash(f"clinic lat/lon updated")
+            return(farm)
+        else:
+            flash('unable to retrieve lat/lon')
+            return(farm)
+
 
 @app.route('/farm/new', methods=['GET', 'POST'])
 @login_required
@@ -26,6 +55,7 @@ def farmNew():
             )
         # This is a method that saves the data to the mongoDB database.
         newFarm.save()
+        newFarm = updateLatLon(newFarm)
         return redirect(url_for('farm',farmId=newFarm.id))
     return render_template('farmform.html',form=form)
 
@@ -35,6 +65,7 @@ def farmEdit(farmId):
     form = FarmForm()  
     thisFarm = Farm.objects.get(id=farmId)
     if form.validate_on_submit():
+        print(thisFarm)
         thisFarm.update(
             streetAddress = form.streetAddress.data,
             zip = form.zipCode.data,
@@ -60,10 +91,12 @@ def farmEdit(farmId):
     form.name.data = thisFarm.name, 
     current_user.id = thisFarm.author,
     
+    editFarm = updateLatLon(editFarm)
     return render_template('farmform.html',form=form)
 
 @app.route('/farm/<farmId>')
 def farm(farmId):
+    print(farmId)
     thisFarm = Farm.objects.get(id=farmId)
     return render_template('farm.html',farm=thisFarm)
 
@@ -80,3 +113,11 @@ def farmDelete(farmId):
 def farmList():
     farms=Farm.objects()
     return render_template('farmlist.html',farms=farms)
+
+@app.route('/farm/map')
+@login_required
+def farmMap():
+
+    farms = Farm.objects()
+
+    return render_template('farmlocator.html',farms=farms)
